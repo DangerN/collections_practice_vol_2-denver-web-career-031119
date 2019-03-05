666d995b47824ea332436c1ea1eec357a8111762
Crystal Neal <cdneal091@gmail.com>
2019-03-05 18:09:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
 # your code goes here
+def begins_with_r(collection)
+  collection.select do | letter |
+    if !(letter.start_with? 'r')
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(collection)
+  arr = []
+  collection.each do |letter|
+    if letter.include? 'a' 
+      arr << letter
+    end
+  end
+  arr
+end
+
+def first_wa(collection)
+  collection.each do |elem|
+    if elem.start_with? 'wa'
+      return elem
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.each.with_index do |elem,index|
+    if !(elem.class == String)
+      array.delete_at(index)
+    end
+  end
+end
+
+def count_elements()
+end
+
+def merge_data()
+end
+
+def find_cool()
+end
+
+def organize_schools()
+end
