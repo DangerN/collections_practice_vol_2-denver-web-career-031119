90f606d28b8e2d34ed783b9a94c8f48843e9107c
Crystal Neal <cdneal091@gmail.com>
2019-03-05 18:36:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,7 @@ end
 def count_elements(array)
   new_arr = []
   array.each do | name |
-    if new_arr.any? {
+    if new_arr.any? { |h| h[:name] == name
       new_arr = << {count : 1, name: name}
     binding.pry
   end
