382c4fca051d6d14f6d8283de997e9b8c66ff7d8
Crystal Neal <cdneal091@gmail.com>
2019-03-05 19:24:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,8 +44,9 @@ end
 
 def merge_data(data_one , data_two)
   data_one.each do | person |
-  
-    binding.pry
+    data_two[person[:first_name]].each do | stat, value |
+      binding.pry
+    end
   end
 end
 
